e92bf579161584991c66a40b8c6a131689cb28eb
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2025-07-23 00:23:10 +0200

    refactor: update HeaderDesktop component and fix font path in globals.css

--- src/components/HeaderDesktop.tsx
+++ src/components/HeaderDesktop.tsx
@@ -1,19 +1,18 @@
-import React from "react";
+import { FC } from "react";
+// import styles from "./HeaderDesktop.module.css";
 
-const HeaderDesktop: React.FC = () => {
+const HeaderDesktop: FC = () => {
   return (
     <header className="w-full bg-background text-foreground p-4">
       <nav className="flex justify-between items-center border-b border-stroke">
         <div className="flex gap-8">
           <div className="text-md p-4">dariusz-wrzesien</div>
-          <div className="p-4">_hello</div>
-          <div className="p-4">_about-me</div>
-          <div className="p-4">_projects</div>
-          <div className="p-4">_cv</div>
+          {/* <div className={styles.navLink}>_hello</div>
+          <div className={styles.navLink}>_about-me</div>
+          <div className={styles.navLink}>_projects</div>
+          <div className={styles.navLink}>_cv</div> */}
         </div>
-        <div>
-          <div>_contact-me</div>
-        </div>
+        <div>{/* <div className={styles.navLink}>_contact-me</div> */}</div>
       </nav>
     </header>
   );
