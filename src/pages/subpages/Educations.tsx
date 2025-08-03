6990a791861dc75e9213574801899eb833d7b494
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2025-08-03 13:37:54 +0200

    feat: update Educations component to use new educations data structure and enhance educational qualifications display

--- src/pages/subpages/Educations.tsx
+++ src/pages/subpages/Educations.tsx
@@ -1,12 +1,12 @@
 import TextBlock from "../../components/TextBlock";
-import bio from "../../data/bio.html?raw";
+import educations from "../../data/educations.html?raw";
 import { useScrollTo } from "../../hooks/use-scrollTo";
 
 const Educations = () => {
   const { ref } = useScrollTo();
   return (
     <div className="flex flex-col overflow-auto custom-scrollbar w-full">
-      <TextBlock ref={ref} text={bio} />
+      <TextBlock ref={ref} text={educations} />
     </div>
   );
 };
