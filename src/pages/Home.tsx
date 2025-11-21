40494fc1a614f1af0dd612661e5e29661fb0a553
Dariusz Wrzesien <dwrzesien@ccaeducate.me>
2025-11-21 21:33:44 +0100

    feat: Enhance Tailwind CSS configuration and update global styles for improved responsiveness
    - Added new screen sizes (xs, sm, md, xl) and extended font sizes in tailwind.config.ts.
    - Updated global styles in globals.css to utilize new font sizes and responsive design for headings and paragraphs.
    - Adjusted Home component styles for better responsiveness across different screen sizes.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,16 @@ import { RiArrowRightSLine } from '@remixicon/react';
 
 const Home = () => {
   return (
-    <div className="custom-sidebar-margin-left px-4 flex gap-4 h-full z-1">
+    <div className="custom-sidebar-margin-left px-2 sm:px-4 flex gap-4 h-full z-1">
       {/* Hello Section */}
       <section className="flex flex-col justify-center w-full">
-        <p className="text-foreground text-lg">Hi all. I am</p>
+        <p className="text-foreground text-sm md:text-lg">Hi all. I am</p>
         <h1 className="text-heading-foreground">Dariusz Wrzesień</h1>
         <h4 className="flex items-center text-indigo-500">
           <RiArrowRightSLine /> Fullstack developer
         </h4>
-        <p className="text-foreground text-md pt-16">// find my profile on LinkedIn</p>
-        <p>
+        <p className="text-foreground text-xs md:text-md pt-8 md:pt-16">// find my profile on LinkedIn</p>
+        <p className="text-sm md:text-base break-all">
           <span className="text-indigo-500">const</span> <span className="text-teal-400">linkedinProfile</span> ={' '}
           <a
             className="text-link-foreground hover:text-link-hover-foreground"
